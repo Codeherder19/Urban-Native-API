e3803bdc7fd2b79cb43993619d99ead0258ee331
Brian Forbes <brianandrewf81@gmail.com>
2021-02-28 16:50:49 -0700

    Confgure knexfile for Heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -9,38 +9,23 @@ module.exports = {
       user: "me",
       password: "",
       database: "urban_native"
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + "/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/seeds"
     }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + "/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/seeds"
     }
   }
 
