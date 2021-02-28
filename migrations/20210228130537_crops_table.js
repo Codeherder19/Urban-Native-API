4a9433b05fd6a55cdd88e67a205920dc88de719e
mattdean <matthewallandean@gmail.com>
2021-02-28 13:35:36 -0700

    Make crops table in db

--- migrations/20210228130537_crops_table.js
+++ migrations/20210228130537_crops_table.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('crops', function(table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('description').notNullable();
+    table.string('sunReqs').notNullable();
+    table.string('sowingMethod').notNullable();
+    table.integer('spread').notNullable();
+    table.integer('rowSpacing').notNullable();
+    table.integer('height').notNullable();
+    table.string('daysToMaturity').notNullable();
+    table.specificType('photoLinks', 'integer ARRAY').notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('crops');
 };
